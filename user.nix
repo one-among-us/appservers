463c595d48502d4b95cdb23b4688b089fa159323
sauricat <linshu1729@protonmail.com>
2024-01-08 18:51:33 -0500

    [U] Add maintainer

--- user.nix
+++ user.nix
@@ -5,12 +5,20 @@
   users.users = {
     shu = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" "lp" "scanner" ];
+      extraGroups = [ "wheel" "docker" ];
       shell = pkgs.fish;
       homeMode = "755";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOtqhzrEH5VnSSxcLn7MJKbCw7QFhQmX8hkSmsEMq8/I shu@iwkr"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQOJJrevWnZsc6KAeQFFRDy7Seun+50d/tjAMLpVlvV shu@wlsn"
+      ];
+    };
+    kuniklo = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "docker" ];
+      homeMode = "755";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQruWFx8FAMFKOVzurMgjhaDOjmVJTN6Fz116B+lvVF kuniklo@Pod042A"
       ];
     };
   };
