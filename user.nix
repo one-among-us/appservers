141754dc6baefe5f3ffb40c2388dd1518b846121
Bᴇɪʏᴀɴ Shu <linshu1729@protonmail.com>
2024-01-26 21:02:40 -0500

    [U] Update user

--- user.nix
+++ user.nix
@@ -11,6 +11,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOtqhzrEH5VnSSxcLn7MJKbCw7QFhQmX8hkSmsEMq8/I shu@iwkr"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQOJJrevWnZsc6KAeQFFRDy7Seun+50d/tjAMLpVlvV shu@wlsn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTbApMlB9EorC78LRaeBclH+iLeE4NqIRkVQxi9jgQ0 shu@penguin"
       ];
     };
     kuniklo = {
