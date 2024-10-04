a76d51daecf49bb9f2ed687d0954b2e038d527e6
sauricat <saurica@kazv.moe>
2024-10-04 08:35:10 -0400

    [+] Add tia-legacy

--- apps/reverse.nix
+++ apps/reverse.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, config, ... }:
 let
   live-website = ./live-website;
+  tia-legacy = ./tia-legacy;
 in
 {
   services.caddy = {
@@ -15,6 +16,30 @@ in
       live.oau.app {
         tls info@oneamongus.ca
         root * ${live-website}
+        file_server
+        handle_errors {
+          @404 {
+            expression {http.error.status_code}==404
+          }
+          rewrite @404 /index.html
+        }
+      }
+
+      oau.edu.kg {
+        tls info@oneamongus.ca
+        root * ${tia-legacy}
+        file_server
+        handle_errors {
+          @404 {
+            expression {http.error.status_code}==404
+          }
+          rewrite @404 /index.html
+        }
+      }
+
+      lib.oau.edu.kg {
+        tls info@oneamongus.ca
+        root * ${tia-legacy}
         file_server
         handle_errors {
           @404 {
