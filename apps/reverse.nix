372d3b00afed7a33f7ea97eff7320af2131447d5
sauricat <saurica@kazv.moe>
2024-10-06 17:20:27 -0400

    [F] Fix redirect

--- apps/reverse.nix
+++ apps/reverse.nix
@@ -7,21 +7,13 @@ in
   services.caddy = {
     enable = true;
     extraConfig = ''
-      transacademic.org {
-        reverse_proxy * https://oau.edu.kg {
-          header_up Host {http.reverse_proxy.upstream.hostport}
-        }
-      }
-
       live.oau.app {
         tls info@oneamongus.ca
         root * ${live-website}
         file_server
         handle_errors {
-          @404 {
-            expression {http.error.status_code}==404
-          }
-          rewrite @404 /index.html
+          redir * /
+          file_server
         }
       }
 
@@ -30,10 +22,8 @@ in
         root * ${tia-legacy}
         file_server
         handle_errors {
-          @404 {
-            expression {http.error.status_code}==404
-          }
-          rewrite @404 /index.html
+          redir * /
+          file_server
         }
       }
 
@@ -42,10 +32,8 @@ in
         root * ${tia-legacy}
         file_server
         handle_errors {
-          @404 {
-            expression {http.error.status_code}==404
-          }
-          rewrite @404 /index.html
+          redir * /
+          file_server
         }
       }
     '';
