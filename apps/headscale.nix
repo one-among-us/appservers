6ab2a3f00a367a93d1e1fc7b6e0a4d019f08f5b4
sauricat <saurica@kazv.moe>
2025-11-21 04:40:23 -0500

    [F] Hotfix headscale

--- apps/headscale.nix
+++ apps/headscale.nix
@@ -6,10 +6,15 @@
 
   services.headscale = {
     enable = true;
-    settings.dns.magic_dns = false;
+    settings = {
+      server_url = "https://hs.oau.app";
+      dns = {
+        magic_dns = false;
+        override_local_dns = false;
+      };
+    };
     address = "0.0.0.0";
     port = 40180;
-    settings.server_url = "https://hs.oau.app";
   };
 
   virtualisation.arion = {
@@ -18,7 +23,7 @@
       headscale-ui.settings.services.headscale-ui.service = {
         image = "ghcr.io/gurucomputing/headscale-ui:latest";
         restart = "unless-stopped";
-        ports = [ "9443:443" ];
+        ports = [ "9443:443" "9090:80" ];
       };
     };
   };
@@ -27,11 +32,7 @@
     enable = true;
     extraConfig = ''
       hs.oau.app {
-        reverse_proxy /web* https://localhost:9443 {
-          transport http {
-            tls_insecure_skip_verify
-          }
-        }
+        reverse_proxy /web* localhost:9090
         reverse_proxy * localhost:40180
       }
     '';
